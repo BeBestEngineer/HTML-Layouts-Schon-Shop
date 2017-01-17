8ad27d11b79c6606e57c5b181d6cffe4a5c29f94
Alexandr Ja <mr-spinn@outlook.com>
2017-01-17 14:58:19 +0600

    disable jquery-ui.js, enable price-regulator.min.js

--- source/other_modules/_end_html/_end_html.php
+++ source/other_modules/_end_html/_end_html.php
@@ -1,6 +1,6 @@
     <script src="global_third_components/libs/jquery/jquery.min.js"></script>
     <script src="global_third_components/frameworks/bootstrap/dist/all-js-modules/bootstrap.min.js"></script>
-    <script src="global_third_components/libs/jquery-ui/js/jquery-ui.js"></script>
+    <script src="global_third_components/libs/jquery-ui/js/price-regulator.min.js"></script>
     <script src="global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.min.js"></script>
 
     <script src="global_js_components/main.js"></script>
