1bde72607f71ed6ffe34bfd1507f01354f677578
Alexandr Ja <mr-spinn@outlook.com>
2017-01-18 16:29:52 +0600

    update markup

--- source/other_modules/_end_html/_end_html.php
+++ source/other_modules/_end_html/_end_html.php
@@ -2,7 +2,6 @@
     <script src="global_third_components/frameworks/bootstrap/dist/all-js-modules/bootstrap.min.js"></script>
     <script src="global_third_components/libs/jquery-ui/js/price-regulator.min.js"></script>
     <script src="global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.min.js"></script>
-
     <script src="global_js_components/main.js"></script>
   </body>
 </html>
